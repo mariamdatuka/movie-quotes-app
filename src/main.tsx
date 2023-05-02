8d158e10720ffd14a19ce2d4ecfd3303ad543e2f
mariamdatuka <mariam.datukishvili.7@gmail.com>
2023-05-02 18:39:12 +0400

    create themeprovider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-
-
+import { ThemeProvider } from '@mui/material';
+import theme from './theme.ts'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+       <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
---
+++ src/theme.ts
@@ -0,0 +1,9 @@
+import { createTheme } from "@mui/material";
+
+let theme=createTheme({
+    palette:{
+         
+    }
+})
+
+export default theme;
